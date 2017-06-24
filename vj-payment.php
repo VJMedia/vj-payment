a2be4549114a01841fcd1f0f246fc310810d7bba
Nakade Hitsujiko <sora8964@gmail.com>
2017-06-25 01:25:05 +0800

    fix exporter

--- vj-payment.php
+++ vj-payment.php
@@ -167,7 +167,29 @@ if($scanresult){
 	}
 	
 	public static function exportcsv() {
-		include("exporter.php");
+		if ( isset( $_POST['vjpayment_downloadcsv'] ) || isset( $_POST['vjpayment_downloadcsvclear'] ) ) {
+		$mode="general";
+		if(isset( $_POST['vjpayment_downloadcsv'])){
+		check_admin_referer( 'vjpayment_downloadcsv', 'vjpayment_downloadcsv' );
+		$clear=false;
+		}elseif(isset( $_POST['vjpayment_downloadcsvclear'] )){
+		check_admin_referer( 'vjpayment_downloadcsvclear', 'vjpayment_downloadcsvclear' );
+		$clear=true;
+		}
+		$status=self::getstatus("general",7000);
+		}elseif ( isset( $_POST['vjpayment_downloadcsv_special'] ) || isset( $_POST['vjpayment_downloadcsvclear_special'] ) ) {
+		$mode="special";
+		if(isset( $_POST['vjpayment_downloadcsv_special'])){
+		check_admin_referer( 'vjpayment_downloadcsv_special', 'vjpayment_downloadcsv_special' );
+		$clear=false;
+		}elseif(isset( $_POST['vjpayment_downloadcsvclear_special'] )){
+		check_admin_referer( 'vjpayment_downloadcsvclear_special', 'vjpayment_downloadcsvclear_special' );
+		$clear=true;
+		}
+		$status=self::getstatus("1000to10",1000);
+		}else{
+		return; //Exit this function
+		}
 		
 		error_reporting(0);
 		
